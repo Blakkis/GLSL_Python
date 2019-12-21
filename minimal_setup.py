a337ac289b4bac9ccc870fdb7b75f6d53dbef4a2
Toni Vuoristo <Greatshader@hotmail.com>
2019-12-21 19:53:32 +0200

    Update minimal_setup.py

--- minimal_setup.py
+++ minimal_setup.py
@@ -1,3 +1,11 @@
+# Note: There was old version which was done wrongly long time ago
+#       Ive noticed a few people using this lately and wanted to fix this
+#       
+#
+#       This is the fixed version where both "vao" and "vbo" are created
+#       Everything else is the same
+
+
 from __future__ import division
 import pygame
 from pygame.locals import *
@@ -13,9 +21,7 @@ from numpy import array
 
 VERTEX_SHADER = """
 #version 330 core
-
 layout(location = 0) in vec3 vPos;
-
 void main()
 {
     gl_Position = vec4(vPos, 1.0);
@@ -24,25 +30,19 @@ void main()
 
 FRAGMENT_SHADER = """
 #version 330 core
-
 #define fragCoord gl_FragCoord.xy
-
 uniform vec2  iMouse;
 uniform float iTime;
 uniform vec2  iResolution;
-
 out vec4 fragColor;
-
 void main()
 {
     // Set origin to center of the screen
     vec2 uv = fragCoord/iResolution.xy * 2.0 - 1.0;
     // Fix aspect ratio
     uv.x *= iResolution.x / iResolution.y;
-
     // Time varying pixel color (Copied from ShaderToy default scene)
     vec3 color = 0.5 + 0.5 * cos(iTime + uv.xyx + vec3(0.0, 2.0, 4.0));
-
     fragColor = vec4(color, 1.0);
 }
 """
@@ -62,7 +62,7 @@ class Main(object):
         # Shader program which hosts the vertex and fragment shader
         self.shader = shaders.compileProgram(self.vertex_shader, self.fragment_shader)
 
-        # Uniform variables
+        # Get the uniform locations
         self.uni_mouse = glGetUniformLocation(self.shader, 'iMouse')
         self.uni_ticks = glGetUniformLocation(self.shader, 'iTime')
 
@@ -76,10 +76,19 @@ class Main(object):
                                 1.0,  1.0, 0.0,
                                -1.0,  1.0, 0.0], dtype='float32')
 
-        self.vertexbuffer = glGenBuffers(1)
-        glBindBuffer(GL_ARRAY_BUFFER, self.vertexbuffer)
+        # Generate VAO
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+
+        # Generate VBO which is stored in the VAO state
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
         glBufferData(GL_ARRAY_BUFFER, self.vertices, GL_STATIC_DRAW)
 
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+
+        
         self.clock = pygame.time.Clock()
 
     def mainloop(self):
@@ -96,17 +105,13 @@ class Main(object):
                     
             glUseProgram(self.shader)
 
+            # Send uniform values
             glUniform2f(self.uni_mouse, *pygame.mouse.get_pos())
             glUniform1f(self.uni_ticks, pygame.time.get_ticks() / 1000.0)
 
-            # Enable Vertex arrays
-            glEnableVertexAttribArray(0)
-            glBindBuffer(GL_ARRAY_BUFFER, self.vertexbuffer)
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
-
+            # Bind the vao (which stores the VBO with all the vertices)
+            glBindVertexArray(self.vao)
             glDrawArrays(GL_QUADS, 0, 4)
-
-            glDisableVertexAttribArray(0) # Optional
 
             pygame.display.set_caption("FPS: {}".format(self.clock.get_fps()))
             pygame.display.flip()
